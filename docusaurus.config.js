cbb82d4c24381681ee559021e53fd08d93a2ac31
Tabea <35971471+tabeaeggler@users.noreply.github.com>
2026-03-18 13:53:08 +0000

    added favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Open Government Data Handbuch - Kanton Zürich',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
